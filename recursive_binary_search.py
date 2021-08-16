732411ebb88188ea3318d32930bed56247a75e4c
Khrystyna <>
2021-08-16 17:30:00 -0500

    recursive binary search update

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -8,11 +8,16 @@ def recursive_binary_search(list, target):
             return True
         else:
             if list[midpoint] > target:
-                recursive_binary_search(list[:midpoint], target)
+                return recursive_binary_search(list[:midpoint], target)
             else:
-                recursive_binary_search(list[midpoint + 1 :], target)
+                return recursive_binary_search(list[midpoint + 1 :], target)
+
+
+def verify(result):
+    print("Target found: ", result)
 
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = recursive_binary_search(numbers, 99)
 
-print(recursive_binary_search(numbers, 12))
+verify(result)
