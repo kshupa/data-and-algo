150367ef962b3b680a32ede9627a1d3c3552d368
christina <>
2021-10-01 15:36:12 -0500

    update linked list

--- linked_list.py
+++ linked_list.py
@@ -21,6 +21,47 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        '''
+        Returns the number of nodes in the list.
+        Takes O(n) time.
+        '''
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.next_node
+        
+        return count
+
+    def add(self, data):
+        '''
+        Adds new Node containing data at head of the list.
+        Takes O(1) time
+        '''
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def search(self, key):
+        '''
+        Search for the first node containing data that matches the key.
+        Return the node or 'None' if not found.
+        Takes O(n) time
+        '''
+
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        
+        return None
+
     def __repr__(self):
         '''
         Returns a string representation of the list.
@@ -41,30 +82,3 @@ class LinkedList:
             current = current.next_node
 
         return '->'.join(nodes)
-
-
-        def is_empty(self):
-            return self.head == None
-
-        
-        def size(self):
-            '''
-            Returns the number of nodes in the list.
-            Takes O(n) time.
-            '''
-            current = self.head
-            count = 0
-            while current:
-                count += 1
-                current = current.next_node
-            
-            return count
-
-    def add(self, data):
-        '''
-        Adds new Node containing data at head of the list.
-        Takes O(1) time
-        '''
-        new_node = Node(data)
-        new_node.next_node = self.head
-        self.head = new_node
